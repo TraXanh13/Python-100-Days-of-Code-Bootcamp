c131587e65ab0d7b53e63ac4b6a90b5a4986a37c
Kyle Duong <kyleduong13@gmail.com>
2023-03-21 23:40:30 -0600

    Made etch-a-sketch

--- Day 19/Etch-A-Sketch/main.py
+++ Day 19/Etch-A-Sketch/main.py
@@ -20,11 +20,19 @@ def right():
     tim.right(10)
 
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 sc.listen()
 sc.onkey(key="w", fun=forward)
 sc.onkey(key="s", fun=backward)
 sc.onkey(key="a", fun=left)
 sc.onkey(key="d", fun=right)
+sc.onkey(key="c", fun=clear)
 
 
 sc.exitonclick()

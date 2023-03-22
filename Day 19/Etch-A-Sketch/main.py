eb056224cdb7c29f956d6053c5a537f495b3784d
Kyle Duong <kyleduong13@gmail.com>
2023-03-21 23:39:28 -0600

    Made etch-a-sketch

---
+++ Day 19/Etch-A-Sketch/main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+sc = Screen()
+
+
+def forward():
+    tim.forward(10)
+
+
+def backward():
+    tim.backward(10)
+
+
+def left():
+    tim.left(10)
+
+
+def right():
+    tim.right(10)
+
+
+sc.listen()
+sc.onkey(key="w", fun=forward)
+sc.onkey(key="s", fun=backward)
+sc.onkey(key="a", fun=right)
+sc.onkey(key="d", fun=left)
+
+
+sc.exitonclick()

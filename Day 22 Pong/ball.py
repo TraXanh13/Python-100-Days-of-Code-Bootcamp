51f371341126af998b895ff73d4a0b75f803fe6b
Kyle Duong <kyleduong13@gmail.com>
2023-04-11 23:59:57 -0600

    Almost done

--- Day 22 Pong/ball.py
+++ Day 22 Pong/ball.py
@@ -16,17 +16,20 @@ class Ball(Turtle):
         self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
 
         if (self.ycor() > 280 or self.ycor() < -280):
-            self.y_move *= -1
+            self.bounce_y()
 
         if (self.xcor() > 380 or self.xcor() < -380):
             self.reset()
 
-    def bounce(self):
+    def bounce_x(self):
         self.x_move *= -1
+
+    def bounce_y(self):
+        self.y_move *= -1
 
     def reset(self):
         self.goto(0, 0)
-        self.bounce()
+        self.bounce_x()
         self.x_move = BALL_SPEED
         self.y_move = BALL_SPEED
 
@@ -36,6 +39,6 @@ class Ball(Turtle):
 
     def paddleCollision(self, paddle):
         if (self.distance(paddle) < 50 and (self.xcor() > 320 or self.xcor() < -320)):
-            self.bounce()
+            self.bounce_x()
             self.speedUp()
             self.move()

382b88ecfc678963b0e59796082dc78dd670ae4b
TraXanh13 <kyleduong13@gmail.com>
2023-04-23 23:56:28 -0600

    Learning pandas

---
+++ Day 25/main.py
@@ -0,0 +1,24 @@
+# CSV: Comma Separated Values
+# import csv
+
+# day, temperature, weather = [], [], []
+# with open("C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/weather_data.csv") as weatherData:
+#     data = csv.reader(weatherData)
+#     for row in data:
+#         if (row[0] != "day"):
+#             day.append(row[0])
+#             temperature.append(row[1])
+#             weather.append(row[2])
+
+# print(day)
+# print(temperature)
+# print(weather)
+
+
+# Pandas
+import pandas
+
+data = pandas.read_csv(
+    "C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/weather_data.csv")
+
+print(data)

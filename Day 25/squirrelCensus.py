31b1690331fb63398669ee823d766f06d36daef6
TraXanh13 <kyleduong13@gmail.com>
2023-04-25 00:00:35 -0600

    Completed Squirrel Census challenge

--- Day 25/squirrelCensus.py
+++ Day 25/squirrelCensus.py
@@ -4,12 +4,12 @@ data = pandas.read_csv(
     "C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 greys = len(data[data["Primary Fur Color"] == "Gray"])
-reds = len(data[data["Primary Fur Color"] == "Cinnamon"])
+cinnamons = len(data[data["Primary Fur Color"] == "Cinnamon"])
 blacks = len(data[data["Primary Fur Color"] == "Black"])
 
 squirrelData = {
-    "Fur Color": ["Grey", "Red", "Black"],
-    "Count": [greys, reds, blacks]
+    "Fur Color": ["Grey", "Cinnamon", "Black"],
+    "Count": [greys, cinnamons, blacks]
 }
 
 squirrelData = pandas.DataFrame(squirrelData)

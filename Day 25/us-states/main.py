6a50ed46787f757ebafc0694fd757518582e28ae
TraXanh13 <kyleduong13@gmail.com>
2023-04-28 03:07:10 -0600

    General screen set up

---
+++ Day 25/us-states/main.py
@@ -0,0 +1,11 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/us-states/blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+screen.exitonclick()

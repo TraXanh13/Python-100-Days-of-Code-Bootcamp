a00026df87bf33e8717a69e144d3261b99e018fe
TraXanh13 <kyleduong13@gmail.com>
2023-05-04 22:54:38 -0600

    Initial game setup

--- Day 25/us-states/main.py
+++ Day 25/us-states/main.py
@@ -1,11 +1,16 @@
-import turtle
+import turtle, pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
 image = "C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/us-states/blank_states_img.gif"
+
+
+
 screen.addshape(image)
 
 turtle.shape(image)
 
-screen.exitonclick()
+answer = screen.textinput(title="Guess the State", prompt="Guess a state's name?")
+
+turtle.mainloop()

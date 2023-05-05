cd1c9357cd862800de69d56b7dbe79161bc2fe2b
Kyle Duong <kyleduong13@gmail.com>
2023-05-05 00:12:49 -0600

    Completed base gameplay of states game

--- Day 25/us-states/main.py
+++ Day 25/us-states/main.py
@@ -1,16 +1,26 @@
 import turtle, pandas
 
+statesGuessed = [];
+answer = ""
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
 image = "C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/us-states/blank_states_img.gif"
 
-
+data = pandas.read_csv("C:/Users/minia/Documents/Projects/Python-100-Days-of-Code-Bootcamp/Day 25/us-states/50_states.csv")
 
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer = screen.textinput(title="Guess the State", prompt="Guess a state's name?")
+stateTurtle = turtle.Turtle()
+stateTurtle.penup()
+stateTurtle.hideturtle()
 
-turtle.mainloop()
+while answer != "Exit" and len(statesGuessed) < 50:
+    answer = screen.textinput(title=f"{len(statesGuessed)}/50 Guess the State", prompt="Guess a state's name?").title()
+
+    if answer in data.state.values:
+        stateTurtle.goto(int(data[data.state == answer].x), int(data[data.state == answer].y))
+        stateTurtle.write(answer)
+        statesGuessed.append(data[data.state == answer])
